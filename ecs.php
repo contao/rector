0a547b30ca65a4d3bb674d22920059a6fbfe0dce
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2023-11-10 18:44:38 +0100

    Add contao/easy-coding-standard to the dev dependencies

---
+++ ecs.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use Symplify\EasyCodingStandard\Config\ECSConfig;
+
+return static function (ECSConfig $ecsConfig): void {
+    $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
+    $ecsConfig->parallel();
+
+    $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
+        'header' => "This file is part of Contao.\n\n(c) Leo Feyer\n\n@license LGPL-3.0-or-later",
+    ]);
+
+    $ecsConfig->cacheDirectory(sys_get_temp_dir().'/rector_ecs_cache');
+};
---
+++ rector.php
@@ -0,0 +1,27 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao.
+ *
+ * (c) Leo Feyer
+ *
+ * @license LGPL-3.0-or-later
+ */
+
+use Rector\Config\RectorConfig;
+
+return static function (RectorConfig $rectorConfig): void {
+    $rectorConfig->sets([__DIR__.'/config/contao.php']);
+
+    $rectorConfig->paths([
+        __DIR__.'/config',
+        __DIR__.'/src',
+        __DIR__.'/ecs.php',
+        __DIR__.'/rector.php',
+    ]);
+
+    $rectorConfig->parallel();
+    $rectorConfig->cacheDirectory(sys_get_temp_dir().'/rector_rector_cache');
+};
