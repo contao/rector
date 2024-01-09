8d54f6ff5fa475627498af6cef9d499defb2b748
Leo Feyer <1192057+leofeyer@users.noreply.github.com>
2024-01-09 11:01:35 +0100

    Update rector/rector to version 0.19

--- config/contao.php
+++ config/contao.php
@@ -10,45 +10,32 @@ declare(strict_types=1);
  * @license LGPL-3.0-or-later
  */
 
-use Contao\Rector\SimplifyObjectOrNullCheckRector;
-use Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector;
-use Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector;
-use Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector;
-use Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector;
-use Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector;
-use Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector;
-use Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector;
-use Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector;
-use Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector;
-use Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector;
 use Rector\Config\RectorConfig;
-use Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector;
-use Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector;
-use Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector;
-use Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector;
 use Rector\Set\ValueObject\SetList;
-use Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector;
 
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->import(SetList::PHP_80);
     $rectorConfig->import(SetList::PHP_81);
 
-    $rectorConfig->rule(ArraySpreadInsteadOfArrayMergeRector::class);
-    $rectorConfig->rule(CompactToVariablesRector::class);
-    $rectorConfig->rule(CountArrayToEmptyArrayComparisonRector::class);
-    $rectorConfig->rule(DisallowedEmptyRuleFixerRector::class);
-    $rectorConfig->rule(JsonThrowOnErrorRector::class);
-    $rectorConfig->rule(NewlineBeforeNewAssignSetRector::class);
-    $rectorConfig->rule(RemoveConcatAutocastRector::class);
-    $rectorConfig->rule(RemoveUnusedPrivateMethodParameterRector::class);
-    $rectorConfig->rule(RestoreDefaultNullToNullableTypePropertyRector::class);
-    $rectorConfig->rule(SimplifyBoolIdenticalTrueRector::class);
-    $rectorConfig->rule(SimplifyDeMorganBinaryRector::class);
-    $rectorConfig->rule(SimplifyEmptyCheckOnEmptyArrayRector::class);
-    $rectorConfig->rule(SimplifyIfReturnBoolRector::class);
-    $rectorConfig->rule(SimplifyObjectOrNullCheckRector::class);
-    $rectorConfig->rule(SymplifyQuoteEscapeRector::class);
-    $rectorConfig->rule(SimplifyUselessVariableRector::class);
+    $rectorConfig->rule(Rector\CodingStyle\Rector\FuncCall\ArraySpreadInsteadOfArrayMergeRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\FuncCall\CompactToVariablesRector::class);
+    $rectorConfig->rule(Rector\CodingStyle\Rector\FuncCall\CountArrayToEmptyArrayComparisonRector::class);
+    $rectorConfig->rule(Rector\Strict\Rector\Empty_\DisallowedEmptyRuleFixerRector::class);
+    $rectorConfig->rule(Rector\Php73\Rector\FuncCall\JsonThrowOnErrorRector::class);
+    $rectorConfig->rule(Rector\CodingStyle\Rector\ClassMethod\NewlineBeforeNewAssignSetRector::class);
+    $rectorConfig->rule(Rector\DeadCode\Rector\Concat\RemoveConcatAutocastRector::class);
+    $rectorConfig->rule(Rector\DeadCode\Rector\ClassMethod\RemoveUnusedPrivateMethodParameterRector::class);
+    $rectorConfig->rule(Rector\CodingStyle\Rector\Stmt\RemoveUselessAliasInUseStatementRector::class);
+    $rectorConfig->rule(Rector\Php74\Rector\Property\RestoreDefaultNullToNullableTypePropertyRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\Identical\SimplifyBoolIdenticalTrueRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\BooleanNot\SimplifyDeMorganBinaryRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\Empty_\SimplifyEmptyCheckOnEmptyArrayRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\If_\SimplifyIfReturnBoolRector::class);
+    $rectorConfig->rule(Rector\CodingStyle\Rector\String_\SymplifyQuoteEscapeRector::class);
+    $rectorConfig->rule(Rector\CodeQuality\Rector\FunctionLike\SimplifyUselessVariableRector::class);
+
+    // Contao
+    $rectorConfig->rule(Contao\Rector\SimplifyObjectOrNullCheckRector::class);
 
     $rectorConfig->phpstanConfig(__DIR__.'/phpstan.neon');
 };
--- ecs.php
+++ ecs.php
@@ -11,15 +11,22 @@ declare(strict_types=1);
  */
 
 use PhpCsFixer\Fixer\Comment\HeaderCommentFixer;
+use SlevomatCodingStandard\Sniffs\Namespaces\ReferenceUsedNamesOnlySniff;
 use Symplify\EasyCodingStandard\Config\ECSConfig;
 
 return static function (ECSConfig $ecsConfig): void {
     $ecsConfig->sets([__DIR__.'/vendor/contao/easy-coding-standard/config/contao.php']);
-    $ecsConfig->parallel();
+
+    $ecsConfig->skip([
+        ReferenceUsedNamesOnlySniff::class => [
+            'config/contao.php',
+        ],
+    ]);
 
     $ecsConfig->ruleWithConfiguration(HeaderCommentFixer::class, [
         'header' => "This file is part of Contao.\n\n(c) Leo Feyer\n\n@license LGPL-3.0-or-later",
     ]);
 
+    $ecsConfig->parallel();
     $ecsConfig->cacheDirectory(sys_get_temp_dir().'/rector_ecs_cache');
 };
